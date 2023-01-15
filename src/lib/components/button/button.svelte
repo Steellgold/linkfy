5152e0b9eaaf8661ed959b196e0ae581fe9dd7e0
Gaëtan H <steellgold0@gmail.com>
2023-01-15 18:50:26 +0100

    🪡 Disabled class

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -9,8 +9,13 @@
     "w-full": size === "large",
     "": size === "default",
   });
+
+  let disabledClass = clsx({
+    "bg-blue-600 hover:bg-blue-700 focus:ring-blue-800": disabled == false,
+    "bg-blue-900 focus:ring-blue-900": disabled == true,
+  });
 </script>
 
-<button on:click type={type} disabled={disabled} class="{sizeClass} text-white focus:ring-2 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 transition-all ease-in-out 3s">
+<button on:click type={type} disabled={disabled} class="{sizeClass} {disabledClass} text-white focus:ring-2 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 transition-all ease-in-out 3s">
   <slot></slot>
 </button>
--- src/lib/components/button/redirect.svelte
+++ src/lib/components/button/redirect.svelte
@@ -10,11 +10,16 @@
     "": size === "default",
   });
 
+  let disabledClass = clsx({
+    "bg-blue-600 hover:bg-blue-700 focus:ring-blue-800": disabled == false,
+    "bg-blue-900 focus:ring-blue-900": disabled == true,
+  });
+
   function clickButton() {
     window.location.href = path;
   }
 </script>
 
-<button on:click={clickButton} type="button" disabled={disabled} class="{sizeClass} text-white focus:ring-2 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 transition-all ease-in-out 3s">
+<button on:click={clickButton} type="button" disabled={disabled} class="{sizeClass} text-white focus:ring-2 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center {disabledClass} transition-all ease-in-out 3s">
   <slot></slot>
 </button>
