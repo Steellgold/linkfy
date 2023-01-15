d3e5c9b9f9716045d8063de6dbd17c52e1252600
Gaëtan H <steellgold0@gmail.com>
2023-01-15 15:38:04 +0100

    🪡 Button and Input component

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  export let type: "button" | "submit" | "reset" = "button";
+  export let disabled: boolean = false;
+  export let size: "large" | "default" = "default";
+
+  const sizeClass = clsx({
+    "w-full": size === "large",
+    "": size === "default",
+  });
+</script>
+
+<button on:click type={type} disabled={disabled} class="{sizeClass} text-white focus:ring-2 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 transition-all ease-in-out 3s">
+  <slot></slot>
+</button>
---
+++ src/lib/components/input/input.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  export let type: "url" | "text" = "url";
+  export let disabled: boolean = false;
+  export let size: "large" | "default" = "default";
+  export let placeholder: string = "";
+  export let id: string = "";
+  export let value: string;
+
+  if (id === "") id = Math.random().toString(36).substring(7);
+
+  const sizeClass = clsx({"w-full": size === "large", "": size === "default"});
+
+  const byDisabled = clsx({
+    "outline-none focus:ring-1 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500": disabled == false,
+    "bg-gray-800 border-gray-700 placeholder-gray-500": disabled == true,
+  });
+
+  function changeValue(event: Event) {
+    const target = event.target as HTMLInputElement;
+    value = target.value;
+  }
+</script>
+
+<input type={type} disabled={disabled} id={id} class="border sm:text-sm rounded-lg block p-2.5 text-white {sizeClass} {byDisabled}" placeholder={placeholder} on:input={changeValue}>
