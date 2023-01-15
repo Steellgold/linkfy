359787c4a5f888550f8d67803882dfa4095c5bb4
Gaëtan H <steellgold0@gmail.com>
2023-01-15 16:53:49 +0100

    🛞 Redirect Button

---
+++ src/lib/components/button/redirect.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  export let path: string = "/";
+  export let disabled: boolean = false;
+  export let size: "large" | "default" = "default";
+
+  const sizeClass = clsx({
+    "w-full": size === "large",
+    "": size === "default",
+  });
+
+  function clickButton() {
+    window.location.href = path;
+  }
+</script>
+
+<button on:click={clickButton} type="button" disabled={disabled} class="{sizeClass} text-white focus:ring-2 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 transition-all ease-in-out 3s">
+  <slot></slot>
+</button>
