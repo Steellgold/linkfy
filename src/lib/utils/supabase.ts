18d488134b8af1f3a7cecd1b1558fa0122be3038
Gaëtan H <steellgold0@gmail.com>
2023-01-15 18:51:20 +0100

    🛰️ `insertLink` function

--- src/lib/utils/supabase.ts
+++ src/lib/utils/supabase.ts
@@ -1,6 +1,9 @@
-import { supabaseUrl, supabaseKey } from "$env/static/private";
 import { createClient } from "@supabase/supabase-js";
+import { supabaseUrl, supabaseKey } from "$env/static/private";
 
 const supabase = createClient(supabaseUrl, supabaseKey)
-
 export const db = supabase;
+
+export function insertLink(link: string, shorted: string) {
+  return supabase.from('links').insert({ link, shorted });
+}
