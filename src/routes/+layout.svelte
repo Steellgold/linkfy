0ad1714c9cdcf855bcc885f7be70b18827326475
Gaëtan H <steellgold0@gmail.com>
2023-01-17 21:17:06 +0100

    🧬 Save the `Visitor ID`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script>
-  import { Toasts } from "$lib/components/layouts/toast";
   import "../app.postcss";
+
+  import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
+  import { pushToast, Toasts } from "$lib/components/layouts/toast";
+  import { onMount } from "svelte";
+  import { getVisitorId, setVisitorId } from "$lib/utils/Stores";
+  import { PUBLIC_FINGERPRINT_API_KEY } from "$env/static/public";
+
+  onMount(() => {
+    if (getVisitorId() !== null) return;
+    const fpPromise = FingerprintJS.load({ apiKey: PUBLIC_FINGERPRINT_API_KEY, region: "eu" })
+
+    fpPromise.then(fp => fp.get()).then(result => { setVisitorId(result.visitorId); }).catch((error) => { 
+      pushToast("Disable your adblocker to get a better experience", "danger");
+     });
+  })
 </script>
 
 <slot />
