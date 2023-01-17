0dfa61b5bb14c2ba8a7feed9ae958b12369ced38
Gaëtan H <steellgold0@gmail.com>
2023-01-17 21:15:23 +0100

    🛞 Remove useless function

--- src/lib/utils/Supabase.ts
+++ src/lib/utils/Supabase.ts
@@ -3,7 +3,3 @@ import { SECRET_SUPABASE_URL, SECRET_SUPABASE_KEY } from "$env/static/private";
 
 const supabase = createClient(SECRET_SUPABASE_URL, SECRET_SUPABASE_KEY)
 export const db = supabase;
-
-export function insertLink(link: string, shorted: string) {
-  return supabase.from('links').insert({ link, shorted });
-}
