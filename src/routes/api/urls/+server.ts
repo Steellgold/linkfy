49675554926b58b1c1b10addf8c7acf4a4cae0f4
Gaëtan H <steellgold0@gmail.com>
2023-01-17 21:16:20 +0100

    🪡 Rename API call

---
+++ src/routes/api/urls/+server.ts
@@ -0,0 +1,21 @@
+import type { RequestEvent } from "./$types";
+import z from "zod";
+import prisma from "$lib/utils/Prisma";
+
+export async function POST({ request }: RequestEvent): Promise<Response> {
+  const values = await request.json();
+
+  const schema = z.object({
+    baseUrl: z.string().url(),
+    shortUrl: z.string().regex(/^[a-zA-Z0-9]+$/),
+    clicksCount: z.number().int(),
+    fromClicks: z.array(z.object({ key: z.string(), value: z.number() })) ?? [],
+    userId: z.string().optional() ?? null,
+    visitorId: z.string(),
+  }).safeParse(values);
+
+  if (!schema.success) return new Response("Invalid data: " + schema.error.message, { status: 400 });
+
+  let send = await prisma.links_dev.create({ data: schema.data });
+  return new Response("OK", { status: 200 });
+}
