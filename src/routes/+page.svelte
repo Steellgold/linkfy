4b44594d1bbb81e87a7230adbce06195ed29dee0
Gaëtan H <steellgold0@gmail.com>
2023-01-17 21:17:23 +0100

    🏳️ Some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,44 +2,38 @@
   import { Button, CopyButton, RedirectButton } from "$lib/components/button";
   import { Input } from "$lib/components/input";
   import { generateShortUrl } from "$lib/utils/Shortener";
-  import { connected, url as StoreURL } from "$lib/utils/Stores";
+  import { connected, getVisitorId } from "$lib/utils/Stores";
   import { PUBLIC_URL } from "$env/static/public";
   import { pushToast } from "$lib/components/layouts/toast";
-    import { copy } from "$lib/utils/Utils";
 
-  // Temporary:
   if (!connected) pushToast("Currently your link will not be able to receive statistics because you are not logged in.", "danger");
 
-  let url: string = "";
+  let url: string = " ";
   let finalUrl: string = "";
-
-  StoreURL.subscribe(value => { url = value; });
 
   async function transform() {
     const input = document.getElementById("shortened") as HTMLInputElement;
     
-    if (url !== "" && !url.startsWith("https://") && !url.startsWith("http://")) {
-      pushToast("The url must start with https:// or http:// and not be empty", "danger");
-      return;
-    }
+    if (url !== "" && !url.startsWith("https://") && !url.startsWith("http://")) return pushToast("The url must start with https:// or http:// and not be empty", "danger");
 
     let shortenedUrl = generateShortUrl();
     finalUrl = PUBLIC_URL + shortenedUrl;
-    let res = await fetch("/api/shorten", {
+    let res = await fetch("/api/urls", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ 
-        url: url,
-        shorted: shortenedUrl,
+      body: JSON.stringify({
+        baseUrl: url,
+        shortUrl: shortenedUrl,
+        clicksCount: 0,
+        fromClicks: [],
+        // userId: null,
+        visitorId: getVisitorId(),
        }),
     });
 
-    if (res.status !== 200) {
-      pushToast("An error has occurred while generating the link", "danger");
-      return;
-    }
+    if (res.status !== 200) return pushToast("An error has occurred while generating the link", "danger");
     
     pushToast("The link has been generated", "success");
     input.value = finalUrl;
@@ -47,10 +41,7 @@
 </script>
 
 <div class="flex flex-col items-center justify-center py-8 mx-auto md:h-screen lg:py-0">
-  <div class="flex items-center justify-center">
-    <!-- For idk -->
-  </div>
-
+  <!-- <div class="flex items-center justify-center"></div> -->
   <div class="w-full p-6 rounded-lg shadow border md:mt-0 sm:max-w-md bg-gray-800 border-gray-700 sm:p-8">
     <h1 class="mb-1 text-xl font-bold md:text-2xl text-white">Shorten your links</h1>
     <p class="text-sm font-thin text-white">When you click on the "Use" button, the link will be shortened and you will be redirected to the link.</p>
--- src/routes/[shorted]/+page.server.ts
+++ src/routes/[shorted]/+page.server.ts
@@ -1,31 +1,16 @@
-import { db } from '$lib/utils/Supabase';
-import { redirect, type LoadEvent } from '@sveltejs/kit';
-import { error as SvelteKitError } from '@sveltejs/kit';
+import { getLink, type ILink } from "$lib/utils/Prisma";
+import { error as SvelteKitError, redirect, type LoadEvent } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }: LoadEvent ) {
-  let shorted: string | undefined = params.shorted;
+  const { shorted } = params;
+  let link: ILink = await getLink(shorted as string);
 
-  let { data: links, error } = await db
-    .from('links')
-    .select('url')
-    .eq('shorted', shorted);
+  if (!link) throw SvelteKitError(404, {
+    message: "Link not found",
+    // @ts-ignore
+    code: 404,
+  });
 
-  if (error) {
-    throw SvelteKitError(500, {
-      message: 'Internal Server Error',
-      // @ts-ignore
-      code: 500,
-    });
-  }
-
-  if (!links || links.length === 0) {
-    throw SvelteKitError(404, {
-      message: 'Not found',
-      // @ts-ignore
-      code: 404,
-    });
-  }
-
-  throw redirect(301, links[0].url);
+  // throw redirect(301, link["baseUrl"]);
 }
