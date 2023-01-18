e568c251ba20995c221d525a45fb4cff0785c06d
Gaëtan H <steellgold0@gmail.com>
2023-01-18 20:21:35 +0100

    🏳️ Fix Vercel failed build

--- src/routes/history/+page.ts
+++ src/routes/history/+page.ts
@@ -1,12 +1,5 @@
-import { connected } from "$lib/utils/Stores";
 import { error as SvelteKitError } from '@sveltejs/kit';
 
 export const load = (() => {
-  if (!connected) {
-    throw SvelteKitError(401, {
-      message: 'Unauthorized',
-      // @ts-ignore
-      code: 401,
-    });
-  }
+  // TODO: Real authentication system
 });
