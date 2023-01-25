6a6c3df269c13bc54e1d4ffacea6bed1a145d9b0
Gaëtan H <steellgold0@gmail.com>
2023-01-25 18:30:24 +0100

    🛣️ Add the `/api/links/all/count`

---
+++ src/routes/api/links/all/count/+server.ts
@@ -0,0 +1,23 @@
+import type { RequestEvent } from "./$types";
+import z from "zod";
+import prisma from "$lib/utils/db/Prisma";
+
+export async function GET({ request }: RequestEvent): Promise<Response> {
+  const url = new URL(request.url);
+  const type = url.searchParams.get("type");
+  const id = url.searchParams.get("id");
+  if (!type || !id) return new Response("Bad Request: Missing type or id", { status: 400 });
+
+  const schema = z.object({
+    type: z.string().regex(/^(userId|visitorId)$/),
+    id: z.string()
+  }).safeParse({ type, id });
+
+  if (!schema.success) return new Response("Bad Request: " + schema.error.message, { status: 400 });
+  const links = await prisma.links.findMany({ where: { [schema.data.type === "userId" ? "userId" : "visitorId"]: schema.data.id } });
+  const total = links.length;
+
+  return new Response(JSON.stringify({
+    total
+  }), { status: 200 });
+}
