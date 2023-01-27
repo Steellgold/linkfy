a0a3924579cabbd64542988c64f866b910fae28a
Gaëtan H <steellgold0@gmail.com>
2023-01-27 20:56:25 +0100

    🧩 Hooks (Auth)

---
+++ src/hooks.server.ts
@@ -0,0 +1,12 @@
+import "$lib/utils/db/Supabase";
+import { getSupabase } from "@supabase/auth-helpers-sveltekit";
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async({ event, resolve }) => {
+  const { session, supabaseClient } = await getSupabase(event);
+
+  event.locals.sb = supabaseClient;
+  event.locals.session = session;
+
+  return resolve(event);
+};
