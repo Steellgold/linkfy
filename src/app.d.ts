0cebb8ade3604f31349f858e2d3a07a1ce20b489
Gaëtan H <steellgold0@gmail.com>
2023-01-28 23:58:23 +0100

    🧩 Update `app.d.ts`

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,18 +1,18 @@
+import type { TypedSupabaseClient } from "@supabase/auth-helpers-sveltekit/dist/types";
+import type { Session } from "@supabase/supabase-js";
+
 declare global {
-  namespace App {
+  declare namespace App {
     interface Error {
       code: number;
     }
-
     interface Locals {
       sb: TypedSupabaseClient
       session: Session | null
     }
-
     interface PageData {
       session: import("@supabase/supabase-js").Session | null
     }
+    // interface Platform {}
   }
 }
-
-export {};
