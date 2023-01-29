cf21b3c5246c0e6b51825a2aebbea73ee1269b6d
Gaëtan H <steellgold0@gmail.com>
2023-01-29 19:19:57 +0100

    🧊 Add icons

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,9 +1,12 @@
 export { default as IconExternalLink } from "./external-link.svelte";
 export { default as IconUnlink } from "./unlink.svelte";
-export { default as IconUserPlus } from "./user-plus.svelte";
 export { default as IconHistory } from "./history.svelte";
 export { default as IconAdjustmentsAlt } from "./adjustments-alt.svelte";
 export { default as IconAlertTriangle } from "./alert-triangle.svelte";
 export { default as IconCopy } from "./copy.svelte";
+export { default as IconLayoutCollage } from "./layout-collage.svelte";
+
+export { default as IconUserPlus } from "./user-plus.svelte";
+export { default as IconUserCircle } from "./user-circle.svelte";
 
 export { default as IconArrowBack } from "./arrow-back.svelte";
