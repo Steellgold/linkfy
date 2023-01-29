d5bf2a0cdc251ac7bad67703ae7e5767b8feb260
Gaëtan H <steellgold0@gmail.com>
2023-01-29 01:43:00 +0100

    🪡 Remove `pro` color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,12 +6,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "pro": {
-          DEFAULT: "#d9b745",
-          "h": "#c9aa3e",
-          "f": "#bda142",
-          "d": "#947d2e"
-        }
+        
       }
     }
   },
