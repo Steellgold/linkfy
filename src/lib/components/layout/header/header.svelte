217e16e125058f65be270df9a384b61fcca00080
Gaëtan H <steellgold0@gmail.com>
2023-01-30 22:53:29 +0100

    🪡 Some changes

--- src/lib/components/layout/header/header.svelte
+++ src/lib/components/layout/header/header.svelte
@@ -10,7 +10,7 @@
   };
 </script>
 
-<nav class="bg-gray-800 lg:mb-5">
+<nav class="bg-gray-900 lg:mb-5">
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
