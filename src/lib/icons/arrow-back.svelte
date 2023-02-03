4a79134622f2403520c3b4f777380b43d564c9ce
Gaëtan H <steellgold0@gmail.com>
2023-02-03 23:32:38 +0100

    🧹 Icons

--- src/lib/icons/arrow-back.svelte
+++ src/lib/icons/arrow-back.svelte
@@ -2,5 +2,3 @@
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1" />
 </svg>
-
-
--- src/lib/icons/check.svelte
+++ src/lib/icons/check.svelte
@@ -2,5 +2,3 @@
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M5 12l5 5l10 -10" />
 </svg>
-
-
