49c5196bdb9772aafa0b45918d5ebe775d5b2835
Gaëtan H <steellgold0@gmail.com>
2023-02-08 00:53:52 +0100

    🧩 Edit actions

--- src/routes/(auth)/app/edit/[slug]/+page.svelte
+++ src/routes/(auth)/app/edit/[slug]/+page.svelte
@@ -4,61 +4,97 @@
   import { Button } from "$lib/components/button";
   import { IconCheck, IconTrash } from "$lib/icons";
   import type { PageData } from './$types';
+  import { PUBLIC_URL } from "$env/static/public";
     import { pushToast } from "$lib/components/layout/toast";
-
+    import { goto } from "$app/navigation";
   export let data: PageData;
 
   let url: string = data.slugData.url;
+  // let slug: string = data.slugData.slug;
 
   async function save() {
     if (url === data.slugData.url) return;
 
-    // Check if the URL is valid
-    if (!url.startsWith("http://") && !url.startsWith("https://")) {
-      return pushToast("The URL must start with http:// or https://", "danger");
-    }
+    const res = await fetch(PUBLIC_URL + "api/link/update", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        slug: data.slugData.slug,
+        data: {
+          url: url
+        },
+      }),
+    });
 
-    // Check if the URL is valid
-    if (url.length > 2048 || url.length < 10) {
-      return pushToast("The URL must be less than 2048 characters and more than 10 characters", "danger");
+    if (res.ok) {
+      pushToast("Link updated successfully", "success");
+    } else {
+      pushToast("Something went wrong", "danger");
+    }
+  }
+
+  let deleted = false;
+  async function deleteLink() {
+    deleted = true;
+    const res = await fetch(PUBLIC_URL + "api/link/delete", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        slug: data.slugData.slug,
+      }),
+    });
+
+    if (res.ok) {
+      pushToast("Link deleted successfully", "success");
+      goto("/");
+    } else {
+      pushToast("Something went wrong", "danger");
     }
   }
 </script>
 
 <Container maxSize="md">
   <div class="mb-5 p-0">
-    <h1 class="mb-1 text-xl font-bold text-white md:text-2xl">Edition</h1>
-    <p class="text-sm font-normal text-white">
-      This link currently redirects to <a href={data.slugData.url} class="text-blue-500 hover:underline">{data.slugData.url}</a>
-    </p>
+    <h1 class="text-2xl font-bold text-white">Edit Link</h1>
+    <p class="text-sm font-normal text-gray-500">Here, you can edit some of the settings for your link.</p>
   </div>
 
   <form class="flex flex-col gap-4">
-    <div>
-      <Input
-        bind:value={url}
-        props={{ placeholder: "https://example.com", size: "small", width: "full" }}
-      />
+    <Input bind:value={url} props={{ placeholder: "https://example.com", size: "small", width: "full" }} />
+
+    <div class="relative w-[250px] sm:w-[405px]" >
+      <span class="text-xs font-medium text-center p-0.5 leading-none rounded-full px-2 bg-pro-900 text-pro-200 absolute -translate-y-1/2 translate-x-1/2 left-auto top-0 right-0">Pro</span>
+      
+      <Input value={data.slugData.slug} props={{
+        placeholder: "https://example.com",
+        size: "small",
+        width: "full",
+        tip: PUBLIC_URL,
+        disabled: true
+      }} />
     </div>
 
     <div class="flex items-center justify-between gap-2 text-sm font-normal">
-      <Button
-        props={{
-          type: "button",
-          size: "large",
-          variant: "blue",
-          withIcon: true,
-        }}
-        on:click={save}
-      >
-        <IconCheck /> Save
-      </Button>
+      {#if !deleted}
+        <Button props={{ type: "button", size: "large", variant: "blue", withIcon: true, }} on:click={save}>
+          <IconCheck /> Save
+        </Button>
 
-      <Button props={{ type: "button", size: "medium", variant: "red" }} >
-        <IconTrash />
-      </Button>
+        <Button props={{ type: "button", size: "medium", variant: "red", disabled: deleted }} on:click={deleteLink}>
+          <IconTrash />
+        </Button>
+      {:else}
+        <div class="mb-3 h-4 w-80 animate-pulse rounded bg-gray-600 p-5" />
+        <div class="mb-3 h-4 w-10 animate-pulse rounded bg-gray-600 p-5" />
+      {/if}
 
+      <!-- TODO: Add a toggle, for "Active/Inactive" -->
       <!-- TODO: Button for access statistics, "PRO" -->
+      <!-- TODO: Add a field "Password protection" pro? if not limit number of links can have? -->
     </div>
   </form>
 </Container>
