188c274f452a064040491244b7687b117777bacd
Gaëtan H <steellgold0@gmail.com>
2023-02-08 14:10:24 +0100

    🛑 Disabled link

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,11 +1,13 @@
 import { PUBLIC_URL } from "$env/static/public";
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async({ params, fetch }) => {
   if (!params.slug) throw redirect(303, PUBLIC_URL);
 
   const data = await fetch(PUBLIC_URL + "api/link?slug=" + params.slug);
+  if (!data.ok) throw error(404, { message: "This link not exist or has been disabled", code: 404 });
+
   const dataJson = await data.json();
 
   if (data.status !== 200) throw redirect(303, PUBLIC_URL);
@@ -24,6 +26,7 @@ export const load = (async({ params, fetch }) => {
   });
 
   return {
-    url: dataJson.url
+    url: dataJson.url,
+    status: dataJson.status
   };
 }) satisfies PageServerLoad;
--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,6 +1,8 @@
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 export const load = (async({ data }) => {
+  if (!data.status) throw error(423, { message: "This link has been disabled", code: 423 });
+
   throw redirect(303, data.url);
 }) satisfies PageLoad;
