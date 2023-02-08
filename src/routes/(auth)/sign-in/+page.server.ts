d07680ee0f847607bc5fd47092ce61a3c5dda184
Gaëtan H <steellgold0@gmail.com>
2023-02-08 16:27:02 +0100

    🚅 Redirect if already connected

--- src/routes/(auth)/sign-in/+page.server.ts
+++ src/routes/(auth)/sign-in/+page.server.ts
@@ -1,7 +1,15 @@
 import { PUBLIC_URL } from "$env/static/public";
 import { AuthApiError } from "@supabase/supabase-js";
 import { fail, redirect } from "@sveltejs/kit";
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load = (async({ locals })  => {
+  if (locals.session?.user) {
+    throw redirect(303, "/");
+  }
+
+  return {};
+}) satisfies PageServerLoad;
 
 export const actions: Actions = {
   login: async({ request, locals, cookies }) => {
