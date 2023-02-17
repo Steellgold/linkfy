b17141a4c48842a6ef04417c60ac9dbfa51194fc
Gaëtan H <steellgold0@gmail.com>
2023-02-17 17:52:18 +0100

    🧩 Testing thing

---
+++ src/routes/[slug]/+page.server.ts
@@ -0,0 +1,21 @@
+import { PUBLIC_URL, PUBLIC_API_URL } from "$env/static/public";
+import type { Link } from "$lib/types/link.type";
+import { restRequest } from "$lib/utils/request/request";
+import { error, redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+// eslint-disable-next-line
+export const load = (async({ params }) => {
+  if (!params.slug) throw redirect(303, PUBLIC_URL);
+  const data = await restRequest<Link>("get", PUBLIC_API_URL + "link/get/" + params.slug + "/url,status");
+
+  if (!data.success || !data.data.status) {
+    throw error(404, { message: "This link not exist or has been disabled", code: 404 });
+  }
+
+  // TODO: Send a "ping" for increase the link's clicks, and other stats/things
+  return {
+    status: data.data.status,
+    url: data.data.url
+  };
+}) satisfies PageServerLoad;
--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,18 +1,8 @@
-import { PUBLIC_URL, PUBLIC_API_URL } from "$env/static/public";
-import type { Link } from "$lib/types/link.type";
-import { restRequest } from "$lib/utils/request/request";
 import { error, redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-// eslint-disable-next-line
-export const load = (async({ params }) => {
-  if (!params.slug) throw redirect(303, PUBLIC_URL);
-  const data = await restRequest<Link>("get", PUBLIC_API_URL + "link/get/" + params.slug + "/url,status");
+export const load = (async({ data }) => {
+  if (!data.status) throw error(423, { message: "This link has been disabled", code: 423 });
 
-  if (!data.success || !data.data.status) {
-    throw error(404, { message: "This link not exist or has been disabled", code: 404 });
-  }
-
-  // TODO: Send a "ping" for increase the link's clicks, and other stats/things
-  throw redirect(303, data.data.url);
+  throw redirect(303, data.url);
 }) satisfies PageLoad;
