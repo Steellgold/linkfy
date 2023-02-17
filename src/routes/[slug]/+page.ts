a9a68fc73ad43abc90cd7cbb819036b4b3f96b95
Gaëtan H <steellgold0@gmail.com>
2023-02-17 15:25:45 +0100

    🧩 Testing thing

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -9,17 +9,10 @@ export const load = (async({ params }) => {
   if (!params.slug) throw redirect(303, PUBLIC_URL);
   const data = await restRequest<Link>("get", PUBLIC_API_URL + "link/get/" + params.slug + "/url,status");
 
-  if (!data.success) {
+  if (!data.success || !data.data.status) {
     throw error(404, { message: "This link not exist or has been disabled", code: 404 });
   }
 
-  try {
-    await fetch(PUBLIC_API_URL + "link/" + params.slug);
-  } catch (e) {
-    console.error(e);
-  }
-
-  if (!data.data.status) throw error(423, { message: "This link has been disabled", code: 423 });
   // TODO: Send a "ping" for increase the link's clicks, and other stats/things
   throw redirect(303, data.data.url);
 }) satisfies PageServerLoad;
