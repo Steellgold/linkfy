049aa80f69237fd03121ad3ca4a1d723cf1edb98
Gaëtan H <steellgold0@gmail.com>
2023-02-17 15:41:30 +0100

    📏 satisfies

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -2,7 +2,7 @@ import { PUBLIC_URL, PUBLIC_API_URL } from "$env/static/public";
 import type { Link } from "$lib/types/link.type";
 import { restRequest } from "$lib/utils/request/request";
 import { error, redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import type { PageLoad } from "./$types";
 
 // eslint-disable-next-line
 export const load = (async({ params }) => {
@@ -15,4 +15,4 @@ export const load = (async({ params }) => {
 
   // TODO: Send a "ping" for increase the link's clicks, and other stats/things
   throw redirect(303, data.data.url);
-}) satisfies PageServerLoad;
+}) satisfies PageLoad;
