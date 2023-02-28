1c9d6f1a2c0aecc851e01582548b8a5d981f6e6e
Gaëtan H <steellgold0@gmail.com>
2023-02-28 21:19:43 +0100

    🎛️ Stat page

---
+++ src/routes/[slug]/stats/+page.svelte
@@ -0,0 +1,51 @@
+<div class="flex flex-col items-center mx-auto px-3 py-4 lg:py-0">
+  <div class="w-full rounded-lg p-4 md:mt-0 max-w-5xl">
+    <div class="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-3">
+      <div class="flex flex-col items-center justify-center h-24 rounded bg-gray-800">
+        <h5 class="flex items-center gap-2 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Total clicks
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="text-green-400 animate-pulse mt-1">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M10.586 3l-6.586 6.586a2 2 0 0 0 -.434 2.18l.068 .145a2 2 0 0 0 1.78 1.089h2.586v2a1 1 0 0 0 1 1h6l.117 -.007a1 1 0 0 0 .883 -.993l-.001 -2h2.587a2 2 0 0 0 1.414 -3.414l-6.586 -6.586a2 2 0 0 0 -2.828 0z" stroke-width="0" fill="currentColor"></path>
+            <path d="M15 20a1 1 0 0 1 .117 1.993l-.117 .007h-6a1 1 0 0 1 -.117 -1.993l.117 -.007h6z" stroke-width="0" fill="currentColor"></path>
+            <path d="M15 17a1 1 0 0 1 .117 1.993l-.117 .007h-6a1 1 0 0 1 -.117 -1.993l.117 -.007h6z" stroke-width="0" fill="currentColor"></path>
+          </svg>
+        </h5>
+        <p class="font-normal flex gap-2 text-gray-400">
+          27,000
+          <span class="text-green-400">+ 2,000 (this day)</span>
+        </p>
+      </div>
+      <div class="flex flex-col items-center justify-center h-24 rounded bg-gray-800">
+        <h5 class="flex items-center gap-2 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Total clicks
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="text-green-400 animate-pulse mt-1">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M10.586 3l-6.586 6.586a2 2 0 0 0 -.434 2.18l.068 .145a2 2 0 0 0 1.78 1.089h2.586v2a1 1 0 0 0 1 1h6l.117 -.007a1 1 0 0 0 .883 -.993l-.001 -2h2.587a2 2 0 0 0 1.414 -3.414l-6.586 -6.586a2 2 0 0 0 -2.828 0z" stroke-width="0" fill="currentColor"></path>
+            <path d="M15 20a1 1 0 0 1 .117 1.993l-.117 .007h-6a1 1 0 0 1 -.117 -1.993l.117 -.007h6z" stroke-width="0" fill="currentColor"></path>
+            <path d="M15 17a1 1 0 0 1 .117 1.993l-.117 .007h-6a1 1 0 0 1 -.117 -1.993l.117 -.007h6z" stroke-width="0" fill="currentColor"></path>
+          </svg>
+        </h5>
+        <p class="font-normal flex gap-2 text-gray-400">
+          27,000
+          <span class="text-green-400">+ 2,000 (this day)</span>
+        </p>
+      </div>
+      <div class="flex flex-col items-center justify-center h-24 rounded bg-gray-800">
+        <h5 class="flex items-center gap-2 mb-2 text-2xl font-bold tracking-tight text-white">
+          Total clicks
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="text-red-400 animate-pulse mt-1">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M9 8l-.117 .007a1 1 0 0 0 -.883 .993v1.999l-2.586 .001a2 2 0 0 0 -1.414 3.414l6.586 6.586a2 2 0 0 0 2.828 0l6.586 -6.586a2 2 0 0 0 .434 -2.18l-.068 -.145a2 2 0 0 0 -1.78 -1.089l-2.586 -.001v-1.999a1 1 0 0 0 -1 -1h-6z" stroke-width="0" fill="currentColor"></path>
+            <path d="M15 2a1 1 0 0 1 .117 1.993l-.117 .007h-6a1 1 0 0 1 -.117 -1.993l.117 -.007h6z" stroke-width="0" fill="currentColor"></path>
+            <path d="M15 5a1 1 0 0 1 .117 1.993l-.117 .007h-6a1 1 0 0 1 -.117 -1.993l.117 -.007h6z" stroke-width="0" fill="currentColor"></path>
+         </svg>
+        </h5>
+        <p class="font-normal flex gap-2 text-gray-400">
+          27,000
+          <span class="text-red-400">+ 2,000 (this day)</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
