092819d8acdcc90c19d72540b9cb1e21eca23418
Gaëtan H <steellgold0@gmail.com>
2023-03-03 22:44:03 +0100

    🎛️ QRCode button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,6 @@
           <Input props={{
             type: "text",
             placeholder: "https://example.com",
-            value: "",
             size: "large"
           }} />
         </div>
@@ -55,6 +54,24 @@
             <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path>
             <path d="M10 14l10 -10"></path>
             <path d="M15 4l5 0l0 5"></path>
+          </svg>
+        </Button>
+  
+        <Button props={{ type: "button", variant: "blue", size: "lg", disabled: true, full: false }}>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-qrcode" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+            <path d="M7 17l0 .01"></path>
+            <path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+            <path d="M7 7l0 .01"></path>
+            <path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+            <path d="M17 7l0 .01"></path>
+            <path d="M14 14l3 0"></path>
+            <path d="M20 14l0 .01"></path>
+            <path d="M14 14l0 3"></path>
+            <path d="M14 20l3 0"></path>
+            <path d="M17 17l3 0"></path>
+            <path d="M20 17l0 3"></path>
           </svg>
         </Button>
       </div>
