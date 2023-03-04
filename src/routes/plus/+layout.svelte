ca62ce9a1efc19a93f0c17473c20023778cfcb82
Gaëtan H <steellgold0@gmail.com>
2023-03-04 20:28:04 +0100

    💲 Pricing page

---
+++ src/routes/plus/+layout.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  let yearly = false;
+</script>
+
+<slot />
+
+<hr class="border-gray-800 mt-8 mb-8 mx-auto max-w-screen-xl w-1/5 border-2 border-dotted" />
+
+<div class="grid grid-cols-1 px-4">
+  <label class="relative inline-flex items-center cursor-pointer mx-auto max-w-screen-xl mb-8">
+    <input type="checkbox" value="" class="sr-only peer" bind:checked={yearly}>
+    <div class="w-11 h-6 peer-focus:outline-none peer-focus:ring-[#F4D562] rounded-full peer bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all border-gray-600 peer-checked:bg-[#F4D562]"></div>
+    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{yearly ? 'Yearly' : 'Monthly'} subscription</span>
+  </label>
+
+  <div class="flex flex-col p-6 mx-auto max-w-lg text-center rounded-lg border shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
+    <h3 class="mb-0 text-2xl font-extrabold pro">Plus</h3>
+    <p class="font-light sm:text-lg text-gray-400 mb-3 w-30">One plan for all the features you need.</p>
+    <div class="flex justify-center items-baseline pb-4">
+      {#if yearly}
+        <div class="flex flex-col items-center">
+          <div class="flex items-baseline">
+            <span class="mr-2 text-5xl font-extrabold">€79.99</span>
+            <span class="text-gray-400">/yearly</span>
+          </div>
+          <span class="mt-3 text-xs font-medium mr-2 px-2.5 py-0.5 rounded bg-[#F4D562] text-gray-800"> -20%</span>
+        </div>
+      {:else}
+        <span class="mr-2 text-5xl font-extrabold">€9.99</span>
+        <span class="text-gray-400">/month</span>
+      {/if}
+    </div>
+
+    <p class="text-gray-500 mb-4 text-sm mx-auto max-w-screen-xl">7 days free trial included in your subscription.</p>
+
+    <button class="mb-1 text-white bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-600 hover:from-yellow-500 hover:to-yellow-700 hover:text-white font-semibold px-6 py-3 rounded-lg shadow">
+      Get started
+    </button>
+    <p class="flex items-center gap-0 text-gray-500 text-sm mt-4 mx-auto max-w-screen-xl">
+      Secure payment with
+      <span class="flex items-center font-bold">
+        <img class="h-6" src="https://cdn.brandfolder.io/KGT2DTA4/at/8vbr8k4mr5xjwk4hxq4t9vs/Stripe_wordmark_-_blurple.svg" alt="Stripe">
+      </span>
+    </p>
+  </div>
+</div>
---
+++ src/routes/plus/options/+page.svelte
@@ -0,0 +1,76 @@
+<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-10 lg:px-12">
+  <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
+    Need more <span class="pro">options</span> ?
+  </h1>
+  <p class="text-lg font-normal lg:text-xl sm:px-16 xl:px-48 text-gray-400">With a Pro account you unlock additional options for your link generation.</p>
+</div>
+
+<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 px-4 mx-auto max-w-screen-xl lg:px-12">
+  <div class="flex flex-col items-center border rounded-lg shadow md:flex-row md:max-w-xl border-gray-700 bg-gray-800 hover:bg-gray-700">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <h5 class="text-2xl font-bold tracking-tight text-white flex items-center gap-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0z"></path>
+          <path d="M15 9h.01"></path>
+        </svg>
+        Password protection
+      </h5>
+    </div>
+  </div>
+
+  <div class="flex flex-col items-center border rounded-lg shadow md:flex-row md:max-w-xl border-gray-700 bg-gray-800 hover:bg-gray-700">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <h5 class="text-2xl font-bold tracking-tight text-white flex items-center gap-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M21 12a9 9 0 1 0 -9.972 8.948c.32 .034 .644 .052 .972 .052"></path>
+          <path d="M12 7v5l2 2"></path>
+          <path d="M18.42 15.61a2.1 2.1 0 0 1 2.97 2.97l-3.39 3.42h-3v-3l3.42 -3.39z"></path>
+        </svg>
+        Expiration
+      </h5>
+    </div>
+  </div>
+
+  <div class="flex flex-col items-center border rounded-lg shadow md:flex-row md:max-w-xl border-gray-700 bg-gray-800 hover:bg-gray-700">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <h5 class="text-2xl font-bold tracking-tight text-white flex items-center gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M6 7h11a2 2 0 0 1 2 2v.5a.5 .5 0 0 0 .5 .5a.5 .5 0 0 1 .5 .5v3a.5 .5 0 0 1 -.5 .5a.5 .5 0 0 0 -.5 .5v.5a2 2 0 0 1 -2 2h-11a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2"></path>
+          <path d="M7 10l0 4"></path>
+          <path d="M10 10l0 4"></path>
+          <path d="M13 10l0 4"></path>
+        </svg>
+        Number of redirects <span class="text-gray-400 text-base">*</span>
+      </h5>
+    </div>
+  </div>
+
+  <div class="flex flex-col items-center border rounded-lg shadow md:flex-row md:max-w-xl border-gray-700 bg-gray-800 hover:bg-gray-700">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <h5 class="text-2xl font-bold tracking-tight text-white flex items-center gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M4 7m0 1a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1v7a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1z"></path>
+          <path d="M7 16v4"></path>
+          <path d="M7.5 16l9 -9"></path>
+          <path d="M13.5 16l6.5 -6.5"></path>
+          <path d="M4 13.5l6.5 -6.5"></path>
+          <path d="M17 16v4"></path>
+          <path d="M5 20h4"></path>
+          <path d="M15 20h4"></path>
+          <path d="M17 7v-2"></path>
+          <path d="M7 7v-2"></path>
+        </svg>
+        Geographical area <span class="text-gray-400 text-base">**</span>
+      </h5>
+    </div>
+  </div>
+</div>
+
+<div class="mx-auto max-w-screen-xl text-center lg:px-12 px-3">
+  <p class="text-center text-gray-400 text-sm mt-4">* If you set a maximum of 5 redirects, your link cannot be redirected more than 5 times.</p>
+  <p class="text-center text-gray-400 text-sm">** If you set the geographical area to "France", your link can only be used by people located in France.</p>
+</div>
