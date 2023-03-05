a78e6eebb2f46046ada8bc1419c0496a34e89faa
Gaëtan H <steellgold0@gmail.com>
2023-03-05 18:08:12 +0100

    🎛️ Add some utils functions

--- src/lib/utils/NumberUtils.ts
+++ src/lib/utils/NumberUtils.ts
@@ -1,8 +1,3 @@
 export function formatNumber(number: number): string {
-  const tier = Math.log10(number) / 3 | 0;
-  if (tier === 0) return number.toString();
-  const suffix = 'kmbtq'[tier - 1];
-  const scale = Math.pow(10, tier * 3);
-  const scaled = number / scale;
-  return scaled.toFixed(1) + suffix;
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
--- src/lib/utils/StringUtils.ts
+++ src/lib/utils/StringUtils.ts
@@ -5,3 +5,22 @@ export function minimize(text: string, maxLength: number) : string {
 
   return text.substring(0, maxLength - 3) + "[...]";
 }
+
+export function hexToRgb(hex: string) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) } : null;
+}
+
+export function rgbToHex(r: number, g: number, b: number) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+export function hexToRgbInstring(rgb: string, includeRgb: boolean = true) {
+  const rgbValues = hexToRgb(rgb);
+  return includeRgb ? `rgb(${rgbValues?.r}, ${rgbValues?.g}, ${rgbValues?.b})` : `${rgbValues?.r}, ${rgbValues?.g}, ${rgbValues?.b}`;
+}
+
+export function rgbToHexInstring(rgb: string, includeHash: boolean = true) {
+  const rgbValues = rgb.replace(/[^\d,]/g, '').split(',');
+  return includeHash ? rgbToHex(parseInt(rgbValues[0]), parseInt(rgbValues[1]), parseInt(rgbValues[2])) : rgbToHex(parseInt(rgbValues[0]), parseInt(rgbValues[1]), parseInt(rgbValues[2])).replace('#', '');
+}
