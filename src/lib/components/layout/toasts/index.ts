6a8e676b8278991ab434190798c51b534aceeb59
Gaëtan H <steellgold0@gmail.com>
2023-03-05 01:56:46 +0100

    Update some things about toasts

--- src/lib/components/layout/toasts/index.ts
+++ src/lib/components/layout/toasts/index.ts
@@ -5,7 +5,7 @@ import type { ToastProps } from './toast.types';
 
 export const toasts = writable<ToastProps[]>([]);
 
-export function pushToast(toast: ToastProps) {
+export function notify(toast: ToastProps) {
   if (get(toasts).length > 0) toasts.set([]);
 
   toasts.set([...get(toasts), toast]);
--- src/lib/components/layout/toasts/toast.types.ts
+++ src/lib/components/layout/toasts/toast.types.ts
@@ -2,5 +2,4 @@ export type ToastProps = {
   type: "success" | "error" | "warning" | "info" | "bug";
   title?: string;
   message: string;
-  duration?: number;
 }
