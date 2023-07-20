0c256f6c5576e53aec8b09ef3118cb5248cb04d5
Gaëtan H <steellgold0@gmail.com>
2023-07-20 17:32:46 +0200

    chore(linkfy.code-workspace): add project folders and exclude supabase/functions folder chore(prisma/schema.prisma): update field name from shortUrl to slug in Link model chore(server.ts): change port variable case from lowercase port to uppercase PORT feat(server.ts): add support for process.env.PORT environment variable chore(app/auth/callback/route.ts): remove unused imports and database queries feat(app/auth/callback/route.ts): update createRouteHandlerClient to use generic type for Database chore(lib/db/database.types.ts): add types for Database and Json chore(supabase/.gitignore): add .branches and .temp to gitignore chore(supabase/config.toml): add configuration for Supabase project chore(supabase/functions/.vscode/extensions.json): add denoland.vscode-deno extension recommendation
    chore(vscode): add Deno settings for Supabase functions
    chore(seed.sql): add seed.sql file

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,21 +1,17 @@
-import { prisma } from "#/lib/db/prisma";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import type { Database } from "#/lib/db/database.types";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export const GET = async(request: Request): Promise<NextResponse> => {
+import type { NextRequest } from "next/server";
+
+export const GET = async(request: NextRequest): Promise<NextResponse> => {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
+    const supabase = createRouteHandlerClient<Database>({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
-
-    const user = (await supabase.auth.getSession()).data.session?.user;
-    if (!user) return NextResponse.redirect(requestUrl.origin);
-
-    const data = await prisma.user.findUnique({ where: { email: user.email } });
-    if (!data && user.email) await prisma.user.create({ data: { email: user.email } });
   }
 
   return NextResponse.redirect(requestUrl.origin);
