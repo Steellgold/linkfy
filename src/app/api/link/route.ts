50b56ff09767351b8002c7f5ac3653a2c3f1a6ba
Gaëtan H <steellgold0@gmail.com>
2023-07-22 14:57:13 +0200

    refactor(route.ts): remove unused imports and add support for additional HTTP methods
    chore(route.ts): add missing newline at end of file

--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -1,13 +1,29 @@
-import type { LinkSchema } from "#/lib/utils/api/schema.user";
-import { linkSchema } from "#/lib/utils/api/schema.user";
-import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { parseBody } from "#/lib/utils/api/parse";
 
-export const POST = async(request: NextRequest): Promise<NextResponse> => {
-  const body = parseBody<LinkSchema>(linkSchema, await request.json());
-  if (body instanceof NextResponse) return NextResponse.json({ message: "Invalid Body" }, { status: 400 });
+export function GET(request: Request): NextResponse {
+  return NextResponse.json({ message: "GET/", origin: request.headers.get("origin") });
+}
 
-  const origin = request.headers.get("origin");
-  return NextResponse.json({ origin });
-};
+export function HEAD(request: Request): NextResponse {
+  return NextResponse.json({ message: "HEAD/", origin: request.headers.get("origin") });
+}
+
+export function POST(request: Request): NextResponse {
+  return NextResponse.json({ message: "POST/", origin: request.headers.get("origin") });
+}
+
+export function PUT(request: Request): NextResponse {
+  return NextResponse.json({ message: "PUT/", origin: request.headers.get("origin") });
+}
+
+export function PATCH(request: Request): NextResponse {
+  return NextResponse.json({ message: "PATCH/", origin: request.headers.get("origin") });
+}
+
+export function DELETE(request: Request): NextResponse {
+  return NextResponse.json({ message: "DELETE/", origin: request.headers.get("origin") });
+}
+
+export function OPTIONS(request: Request): NextResponse {
+  return NextResponse.json({ message: "OPTIONS/", origin: request.headers.get("origin") });
+}
