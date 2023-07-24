cb4a866d0e7b99306fcad93c4b6c6ccdb5efb0b2
Gaëtan H <steellgold0@gmail.com>
2023-07-24 14:58:28 +0200

    fix(page.tsx): change import statement from createServerComponentClient to createClientComponentClient fix(history/page.tsx): change import statement from createServerComponentClient to createClientComponentClient

--- src/app/(user)/history/page.tsx
+++ src/app/(user)/history/page.tsx
@@ -6,13 +6,12 @@ import clsx from "clsx";
 import { LinksTable } from "#/app/_components/link-table";
 import type { ReactElement } from "react";
 import { linksSchema, type LinksResponseSchema } from "#/lib/utils/api/schema.user";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import type { Database } from "#/lib/db/database.types";
-import { cookies } from "next/headers";
 import { prisma } from "#/lib/db/prisma";
 
 async function getData(): Promise<LinksResponseSchema> {
-  const supabase = createServerComponentClient<Database>({ cookies });
+  const supabase = createClientComponentClient<Database>();
   const { data: { user } } = await supabase.auth.getUser();
 
   if (!user) return { links: [], userId: null };
