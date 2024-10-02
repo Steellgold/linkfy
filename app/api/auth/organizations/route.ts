ee6faee3b16b93d06c108290042d7113162b7883
Gaëtan H <gaetanhus@gmail.com>
2024-10-02 17:07:57 +0200

    feat(route.ts): add new route for fetching organization details and members feat(auth.ts): extend NextAuth session and user interfaces to include organization information and update jwt and session callbacks to include organization data in the token and session respectively

---
+++ app/api/auth/organizations/route.ts
@@ -0,0 +1,45 @@
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { NextAuthRequest } from "next-auth/lib";
+import { NextResponse } from "next/server";
+
+export const GET = auth(async(req: NextAuthRequest) => {
+  if (!req) {
+    return NextResponse.json(
+      { error: "You must be signed in to view this page." },
+      { status: 401 }
+    );
+  }
+
+
+  const url = new URL(req.url);
+  const organizationId = url.searchParams.get("id");
+  if (!organizationId) {
+    return NextResponse.json(req.auth?.user.organizations);
+  }
+
+  const organization = req.auth?.user.organizations.find((org) => org.id === organizationId);
+  if (organization) {
+    return NextResponse.json(organization);
+  }
+
+  const prismaOrganization = await prisma.organization.findFirst({
+    where: {
+      id: organizationId,
+      members: {
+        some: {
+          userId: req.auth?.user.id,
+        },
+      },
+    },
+  });
+
+  if (!prismaOrganization) {
+    return NextResponse.json(
+      { error: "You are not a member of this organization." },
+      { status: 401 }
+    );
+  }
+  
+  return NextResponse.json(prismaOrganization);
+})
--- auth.ts
+++ auth.ts
@@ -1,8 +1,22 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import authConfig from "./auth.config"
 import { prisma } from "./lib/prisma"
+import { Organization } from "@prisma/client"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      organizations: Organization[]
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    organizations: Organization[]
+  }
+}
 
 export const {
   handlers,
@@ -14,4 +28,36 @@ export const {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
+  callbacks: {
+    async jwt({ token, user }): Promise<typeof token> {
+      if (user) {
+        const organizations = await prisma.organization.findMany({
+          where: {
+            members: {
+              some: {
+                userId: user.id,
+              },
+            },
+          },
+          select: {
+            id: true,
+            name: true,
+            plan: true,
+          },
+        });
+
+        token.id = user.id;
+        token.organizations = organizations;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.organizations = token.organizations as Organization[];
+      }
+      return session;
+    },
+  }
 })
