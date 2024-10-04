604ac1334c6bddcf72935e734fe619cb5f877c3b
Gaëtan H <gaetanhus@gmail.com>
2024-10-04 02:53:40 +0200

    feat(link.ts): add extractDomain function to extract domain from a given URL for further processing

--- lib/link.ts
+++ lib/link.ts
@@ -2,3 +2,8 @@ export const randomString = (length: number) => {
   const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
 };
+
+export const extractDomain = (url: string) => {
+	const match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)/);
+	return match ? match[1] : null;
+}
