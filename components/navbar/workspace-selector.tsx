549f69ed469ae5e8e4e8d72c6527df569d05927d
Gaëtan H <gaetanhus@gmail.com>
2024-10-05 16:34:17 +0200

    style(workspace-selector.tsx): organize imports and add missing imports for SelectGroup, SelectSeparator, and HousePlus feat(workspace-selector.tsx): add logic to redirect to "/new-workspace" when selecting "Create a workspace" option in the workspace selector feat(workspace-selector.tsx): add condition to hide workspace selector when on "/new-workspace" page

--- components/navbar/workspace-selector.tsx
+++ components/navbar/workspace-selector.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
-import { Skeleton } from "../ui/skeleton";
-import { buttonVariants } from "../ui/button";
-import { useSession } from "next-auth/react";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectSeparator, SelectTrigger, SelectValue } from "../ui/select";
 import { useGetWorkspaces } from "@/lib/actions/organization/workspace.hook";
-import { ReactElement } from "react";
 import { useWorkspaceStore } from "@/lib/store/workspace.store";
+import { buttonVariants } from "../ui/button";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { Skeleton } from "../ui/skeleton";
+import { HousePlus } from "lucide-react";
+import { ReactElement } from "react";
 import Link from "next/link";
 
 export const WorkspaceSelector = (): ReactElement => {
   const { status: userStatus } = useSession();
   const { status, data } = useGetWorkspaces();
   const { selectedWorkspaceId, setSelectedWorkspaceId } = useWorkspaceStore();
+  const path = usePathname();
 
   if (status == "pending" || status == "error") {
     return <Skeleton className="w-32 h-8" />
@@ -22,20 +25,41 @@ export const WorkspaceSelector = (): ReactElement => {
     setSelectedWorkspaceId(data[0].id);
   }
 
+  if (path == "/new-workspace") return <></>;
+
   return (
     <>
       {data.length > 0 ? (
         <>
-          <Select onValueChange={(value) => setSelectedWorkspaceId(value)} defaultValue={selectedWorkspaceId ?? (data.length > 0 ? data[0].id : undefined)}>
+          <Select open onValueChange={(value: string) => {
+            if (value == "new-workspace") {
+              window.location.href = "/new-workspace";
+              return;
+            }
+
+            setSelectedWorkspaceId(value);
+          }} defaultValue={selectedWorkspaceId ?? (data.length > 0 ? data[0].id : undefined)}>
             <SelectTrigger className="w-[260px]">
               <SelectValue placeholder="Select a workspace" />
             </SelectTrigger>
+            
             <SelectContent>
-              {data.map(org => (
-                <SelectItem key={org.id} value={org.id}>
-                  {org.name}
-                </SelectItem>
-              ))}
+              <SelectGroup>
+                {data.map(org => (
+                  <SelectItem key={org.id} value={org.id}>
+                    {org.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+
+              <SelectSeparator />
+
+              <SelectItem value="new-workspace">
+                <span className="flex items-center">
+                  <HousePlus className="w-4 h-4 mr-2" />
+                  Create a workspace
+                </span>
+              </SelectItem>
             </SelectContent>
           </Select>
         </>
